d996a3ad9fd013e82762eb92d910a115c32c0f37
karyukawamura <karyukawamura@gmail.com>
2019-07-16 18:28:51 +0800

    ?

--- Javascript/main.js
+++ Javascript/main.js
@@ -21,3 +21,51 @@ $(function() {
   });
   $(".open-menu").height($(window).height());
 });
+
+// Vanilla JavaScript Scroll to Anchor
+// @ https://perishablepress.com/vanilla-javascript-scroll-anchor/
+
+(function() {
+  scrollTo();
+})();
+
+function scrollTo() {
+  const links = document.querySelectorAll(".scroll");
+  links.forEach(each => (each.onclick = scrollAnchors));
+}
+
+function scrollAnchors(e, respond = null) {
+  const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+  e.preventDefault();
+  var targetID = respond
+    ? respond.getAttribute("href")
+    : this.getAttribute("href");
+  const targetAnchor = document.querySelector(targetID);
+  if (!targetAnchor) return;
+  const originalTop = distanceToTop(targetAnchor);
+  window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
+  const checkIfDone = setInterval(function() {
+    const atBottom =
+      window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+    if (distanceToTop(targetAnchor) === 0 || atBottom) {
+      targetAnchor.tabIndex = "-1";
+      targetAnchor.focus();
+      window.history.pushState("", "", targetID);
+      clearInterval(checkIfDone);
+    }
+  }, 100);
+}
+const current = document.querySelector("#current");
+const imgs = document.querySelectorAll(".imgs img");
+const opacity = 0.6;
+
+imgs[0].style.opacity = opacity;
+imgs.forEach(img => img.addEventListener("click", imgclick));
+
+function imgclick(e) {
+  imgs.forEach(img => (img.style.opacity = 1));
+  current.src = e.target.src;
+  current.classList.add("fade-in");
+  setTimeout(() => current.classList.remove("fade-in"), 500);
+  e.target.style.opacity = opacity;
+}
